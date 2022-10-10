1f08e4e3b10982be5c7af6529ea3e3597304ac88
Kamrul hasan <kamrulhasan275723@gmail.com>
2022-10-10 09:46:39 +0600

    problem

--- resources/views/student/projectSubmission.blade.php
+++ resources/views/student/projectSubmission.blade.php
@@ -4,7 +4,7 @@
 
 <h2 class="text-center my-5">Project Submission Form</h2>
 
-<form action="{{ url('store-project-submission') }}" method="post" class="bg-dark text-white p-4">
+<form action="{{ url('store-project-submission') }}" method="post" class="bg-dark text-white p-4 w-75 m-auto">
   @csrf
 
   <div class="mb-3">
@@ -39,8 +39,8 @@
     </div>
 
   </div>
-  <div id="one" class="mb-3">
-    
+  <div id="one" class="mb-3 w-100">
+
   </div>
 
   <button class="btn btn-primary" type="submit">Submit</button>
@@ -51,17 +51,14 @@
     const one = document.getElementById('one');
     one.innerHTML = `
     <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Id</label>
-      <input type="number" name="id" class="form-control" id="exampleFormControlInput1">
+      <label for="exampleFormControlInput1" class="form-label">Member Details</label>
+      <div class="d-flex justify-content-between">
+        <input placeholder="Enter Id" type="number" name="id" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter name" type="text" name="name" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter email" type="email" name="email" class="form-control w-25" id="exampleFormControlInput1">
+      </div>
     </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Name</label>
-      <input type="text" name="name" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Email</label>
-      <input type="email" name="email" class="form-control" id="exampleFormControlInput1">
-    </div>
+   
     `
   }
 
@@ -70,28 +67,20 @@
     const one = document.getElementById('one');
     one.innerHTML = `
     <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Id</label>
-      <input type="number" name="id" class="form-control" id="exampleFormControlInput1">
+      <label for="exampleFormControlInput1" class="form-label">First Member Details</label>
+      <div class="d-flex justify-content-between">
+        <input placeholder="Enter id" type="number" name="id" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter name" type="text" name="name" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter email" type="email" name="email" class="form-control w-25" id="exampleFormControlInput1">
+      </div>
     </div>
     <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Name</label>
-      <input type="text" name="name" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Email</label>
-      <input type="email" name="email" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Id</label>
-      <input type="number" name="id" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Name</label>
-      <input type="text" name="name" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Email</label>
-      <input type="email" name="email" class="form-control" id="exampleFormControlInput1">
+      <label for="exampleFormControlInput1" class="form-label">Second Member Details</label>
+      <div class="d-flex justify-content-between">
+        <input placeholder="Enter id" type="number" name="id" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter name" type="text" name="name" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter email" type="email" name="email" class="form-control w-25" id="exampleFormControlInput1">
+      </div>
     </div>
     `
   }
@@ -99,40 +88,28 @@
     const one = document.getElementById('one');
     one.innerHTML = `
     <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Id</label>
-      <input type="number" name="id" class="form-control" id="exampleFormControlInput1">
+      <label for="exampleFormControlInput1" class="form-label">First Member Details</label>
+      <div class="d-flex justify-content-between">
+        <input placeholder="Enter id" type="number" name="id" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter name" type="text" name="name" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter email" type="email" name="email" class="form-control w-25" id="exampleFormControlInput1">
+      </div>
     </div>
     <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Name</label>
-      <input type="text" name="name" class="form-control" id="exampleFormControlInput1">
+      <label for="exampleFormControlInput1" class="form-label">Second Member Details</label>
+      <div class="d-flex justify-content-between">
+        <input placeholder="Enter id" type="number" name="id" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter name" type="text" name="name" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter email" type="email" name="email" class="form-control w-25" id="exampleFormControlInput1">
+      </div>
     </div>
     <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Email</label>
-      <input type="email" name="email" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Id</label>
-      <input type="number" name="id" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Name</label>
-      <input type="text" name="name" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Email</label>
-      <input type="email" name="email" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Id</label>
-      <input type="number" name="id" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Name</label>
-      <input type="text" name="name" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Email</label>
-      <input type="email" name="email" class="form-control" id="exampleFormControlInput1">
+      <label for="exampleFormControlInput1" class="form-label">Third Member Details</label>
+      <div class="d-flex justify-content-between">
+        <input placeholder="Enter id" type="number" name="id" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter name" type="text" name="name" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter email" type="email" name="email" class="form-control w-25" id="exampleFormControlInput1">
+      </div>
     </div>
     `
   }
@@ -140,52 +117,36 @@
     const one = document.getElementById('one');
     one.innerHTML = `
     <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Id</label>
-      <input type="number" name="id" class="form-control" id="exampleFormControlInput1">
+      <label for="exampleFormControlInput1" class="form-label">First Member Details</label>
+      <div class="d-flex justify-content-between">
+        <input placeholder="Enter id" type="number" name="id" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter name" type="text" name="name" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter email" type="email" name="email" class="form-control w-25" id="exampleFormControlInput1">
+      </div>
     </div>
     <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Name</label>
-      <input type="text" name="name" class="form-control" id="exampleFormControlInput1">
+      <label for="exampleFormControlInput1" class="form-label">Second Member Details</label>
+      <div class="d-flex justify-content-between">
+        <input placeholder="Enter id" type="number" name="id" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter name" type="text" name="name" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter email" type="email" name="email" class="form-control w-25" id="exampleFormControlInput1">
+      </div>
     </div>
     <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Email</label>
-      <input type="email" name="email" class="form-control" id="exampleFormControlInput1">
+      <label for="exampleFormControlInput1" class="form-label">Third Member Details</label>
+      <div class="d-flex justify-content-between">
+        <input placeholder="Enter id" type="number" name="id" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter name" type="text" name="name" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter email" type="email" name="email" class="form-control w-25" id="exampleFormControlInput1">
+      </div>
     </div>
     <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Id</label>
-      <input type="number" name="id" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Name</label>
-      <input type="text" name="name" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Email</label>
-      <input type="email" name="email" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Id</label>
-      <input type="number" name="id" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Name</label>
-      <input type="text" name="name" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Email</label>
-      <input type="email" name="email" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Id</label>
-      <input type="number" name="id" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Name</label>
-      <input type="text" name="name" class="form-control" id="exampleFormControlInput1">
-    </div>
-    <div class="mt-3">
-      <label for="exampleFormControlInput1" class="form-label">Email</label>
-      <input type="email" name="email" class="form-control" id="exampleFormControlInput1">
+      <label for="exampleFormControlInput1" class="form-label">Fourth Member Details</label>
+      <div class="d-flex justify-content-between">
+        <input placeholder="Enter id" type="number" name="id" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter name" type="text" name="name" class="form-control w-25" id="exampleFormControlInput1">
+        <input placeholder="Enter email" type="email" name="email" class="form-control w-25" id="exampleFormControlInput1">
+      </div>
     </div>
     `
   }
